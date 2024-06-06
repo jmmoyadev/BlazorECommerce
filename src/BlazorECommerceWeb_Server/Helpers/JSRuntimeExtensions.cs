63a07c1097aa874282a7d0f10bdb07565aaf08c2
Josemi Moya <jmmoyadev@gmail.com>
2024-06-06 20:14:23 +0200

    ✨ JSRuntime Extensions

---
+++ src/BlazorECommerceWeb_Server/Helpers/JSRuntimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorECommerceWeb_Server.Helpers
+{
+    public static class JSRuntimeExtensions
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("showToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("showToastr", "error", message);
+        }
+    }
+}
