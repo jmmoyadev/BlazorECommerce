365563de74fa5374297e0ab24041e6fe987435be
Josemi Moya <jmmoyadev@gmail.com>
2024-06-06 18:50:17 +0200

    ✨ Add Toastr JS

---
+++ src/BlazorECommerceWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿window.showToastr = (type, message) => {
+    if (type == "success") {
+        toastr.success(message, "Operation successful", { timeout: 5000 });
+    }
+    if (type == "error") {
+        toastr.error(message, "Operation failed", { timeout: 5000 });
+    }
+}
